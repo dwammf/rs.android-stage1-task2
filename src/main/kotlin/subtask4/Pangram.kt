06e369961bb492641f34baf579f1e2f076ee830f
dwammf <MarVlDr.j@gmail.com>
2020-04-14 23:47:44 +0300

    feat: add solution to task4

--- src/main/kotlin/subtask4/Pangram.kt
+++ src/main/kotlin/subtask4/Pangram.kt
@@ -2,7 +2,26 @@ package subtask4
 
 class Pangram {
 
+    private val alphabet = "qwertyuiopasdfghjklzxcvbnm"
+    private val vowels = "eyuioa"
+    private val consonants = "qwrtpsdfghjklzxcvbnm"
+
     fun getResult(inputString: String): String {
-        throw NotImplementedError("Not implemented")
+        val words = inputString.split(" ").filterNot{ it == "" || it == "\n" }
+        return  answerForInputString(words, if (isPangram(inputString)) vowels else consonants)
+    }
+
+    private fun answerForInputString(words: List<String>, sortBy: String): String {
+        var sortedList = words.sortedBy {e -> e.count { sortBy.contains(it, true)} }
+        sortBy.forEach { consonant -> sortedList =
+            sortedList.map { element -> element.replace(consonant, consonant.toUpperCase()) }
+        }
+        return sortedList.joinToString(" ")
+        { s -> "${s.count {sortBy.contains(it, true)}}$s" }
+    }
+
+
+    private fun isPangram(str: String): Boolean {
+        return (alphabet.filter { str.contains(it, true) } == alphabet);
     }
 }
